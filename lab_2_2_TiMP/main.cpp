fb1ad074278edf0c3165ec9249b81340b45676f5
Ixonite <anton.tl@yandex.ru>
2019-06-26 11:51:36 +0300

    lab_2

---
+++ lab_2_2_TiMP/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cctype>
+#include <codecvt>
+#include <locale>
+#include "modAlphaCipher.h"
+using namespace std;
+
+void check(const string& Text, const string& key, const bool destructCipherText=false)
+{
+	try {
+		locale loc("ru_RU.UTF-8"); 
+		wstring_convert<codecvt_utf8<wchar_t>, wchar_t> codec; 
+		string cipherText;
+		string decryptedText;
+		modAlphaCipher cipher(key);
+		cipherText = cipher.encrypt(Text);
+		cipherText = cipher.encrypt(Text);
+		if (destructCipherText) {
+			wstring ws = cipher.towstr(cipherText);
+			ws.front() = tolower(ws.front(),loc);
+			cipherText = cipher.fromwstr(ws);
+		}
+		decryptedText = cipher.decrypt(cipherText);
+		cout<<"key="<<key<<endl;
+		cout<<Text<<endl;
+		cout<<cipherText<<endl;
+		cout<<decryptedText<<endl;
+	} catch (const cipher_error & e) {
+		cerr<<"Error: "<<e.what()<<endl;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	check("АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ","Просто");
+	check("АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ","");
+	check("АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ","АБВГ123");
+	check("Абвг Деёж Зиклм Нопрст Уфхцчшщ ЪыЬ Эюя","Супер");
+	check("123","АБВГ");
+	check("АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ","СЕКРЕТНО",true);
+	return 0;
+}
